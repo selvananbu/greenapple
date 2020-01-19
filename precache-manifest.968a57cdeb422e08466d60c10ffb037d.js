25a8120d4a656b1e0ed3678293d44194bcfb697d
Selvan Anbu <anbu.selvan@lisec.com>
2020-01-19 16:23:50 +0400

    Updates

---
+++ precache-manifest.968a57cdeb422e08466d60c10ffb037d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07a9db9fa51e33024a9fb5004bc49b76",
+    "url": "“https://selvananbu.github.io/greenapple/index.html"
+  },
+  {
+    "revision": "d8dde29b713294dbfc4f",
+    "url": "“https://selvananbu.github.io/greenapple/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "88025ba9f596e566f0fe",
+    "url": "“https://selvananbu.github.io/greenapple/static/js/2.b86a68c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "“https://selvananbu.github.io/greenapple/static/js/2.b86a68c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d8dde29b713294dbfc4f",
+    "url": "“https://selvananbu.github.io/greenapple/static/js/main.12435208.chunk.js"
+  },
+  {
+    "revision": "76dabb4b970064a475f1",
+    "url": "“https://selvananbu.github.io/greenapple/static/js/runtime-main.23cde878.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "“https://selvananbu.github.io/greenapple/static/media/logo.5d5d9eef.svg"
+  }
+]);
